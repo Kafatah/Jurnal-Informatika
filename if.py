8cddb30460ff7a941d7a63d11891bccded21f23e
Kafatah <faizataullaha@gmail.com>
2024-11-11 17:36:25 +0700

    Add files via upload

---
+++ if.py
@@ -0,0 +1,34 @@
+# tehe = int(input('masukan angka :'))
+# if tehe >= 0 :
+#     print ('Bilangan positif')
+# else :
+#     print ('Bilangan negatif')
+
+# print ('===Akhir dari program===')
+
+
+# gangen = int(input('masukan angka :'))
+# if gangen % 2 == 0 :
+#       print ('Genap')
+# else :
+#      print ('Ganjil')  
+
+# print ('===Akhir dari program===')
+
+
+# umur = int(input('masukan umur :'))
+# if umur >= 17 :
+#     print ('Selamat kamu sudah cukup umur!!!')
+# else :
+#     print('Sayang sekali kamu belum cukup umur!!!, PERGI SANA')
+    
+# print ('===Akhir dari program===')
+
+
+# diskon = int(input('masukan total harga anda :'))
+# if diskon > 500000 :
+#     print ('selamat total belanja mu menjadi Rp.',diskon - (diskon * 10/100))
+# else :
+#     print ('Selamat total belanja mu menjadi Rp.',diskon - (diskon * 5/100))
+    
+# print ('===Akhir dari program===')
